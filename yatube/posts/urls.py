ef0947b2f0ad867378af6c2787cd0a3a89a4b346
AmidamaruPWNZ <nikita14915@gmail.com>
2024-04-11 18:30:12 +0500

    data base added

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from . import views
 
 
+app_name = 'posts'
 urlpatterns = [
-    path('', views.index),
-    path('group/', views.group_posts),
-    path('group/<int:pk>/', views.post_detail),
+    path('', views.index, name='main_page'),
+    path('group', views.group_posts, name='group_list'),
+    path('group/<slug:slug>/', views.group_posts, name='group_posts'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
 
 def index(request):
-    return HttpResponse('Главная страница')
+    title = 'YaTube'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {'title': title,
+               'text': 'Главная страница проекта YaTube',
+               'posts': posts,}
+    template = 'posts/index.html'
+    return render(request, 'posts/index.html', context)
 
-def group_posts(request):
-    return HttpResponse('Страница с постами')
 
-def post_detail(request, pk):
-    return HttpResponse(f'Пост номер {pk}')
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
+
